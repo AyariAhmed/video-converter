425035770b6b9822fdf8b312215c5c2fd74510fa
AyariAhmed <ayari17@hotmail.fr>
2023-01-16 04:23:57 +0100

    add argo cd

---
+++ infrastructure/terraform/3-monitoring/main.tf
@@ -0,0 +1,34 @@
+resource "kubernetes_namespace" "argocd" {
+  metadata {
+    name = var.kubernetes_argocd_namespace
+  }
+}
+
+resource "helm_release" "argocd" {
+  depends_on = [kubernetes_namespace.argocd]
+
+  name       = "argocd"
+  repository = data.helm_repository.argo.metadata[0].name
+  chart      = "argo-cd"
+  namespace  = var.kubernetes_argocd_namespace
+  version    = var.argocd_helm_chart_version == "" ? null : var.argocd_helm_chart_version
+
+  values = [
+    templatefile(
+      "${path.module}/templates/values.yaml.tpl",
+      {
+        "argocd_server_host"          = var.argocd_server_host
+        "eks_iam_argocd_role_arn"     = data.aws_iam_role.argocd.arn
+        "argocd_github_client_id"     = var.argocd_github_client_id
+        "argocd_github_client_secret" = var.argocd_github_client_secret
+        "argocd_github_org_name"      = var.argocd_github_org_name
+
+        "argocd_ingress_enabled"                 = var.argocd_ingress_enabled
+        "argocd_ingress_tls_acme_enabled"        = var.argocd_ingress_tls_acme_enabled
+        "argocd_ingress_ssl_passthrough_enabled" = var.argocd_ingress_ssl_passthrough_enabled
+        "argocd_ingress_class"                   = var.argocd_ingress_class
+        "argocd_ingress_tls_secret_name"         = var.argocd_ingress_tls_secret_name
+      }
+    )
+  ]
+}
---
+++ infrastructure/terraform/4-git_ops/main.tf
@@ -0,0 +1,13 @@
+resource "helm_release" "argocd" {
+  name = "argocd"
+
+  repository       = "https://argoproj.github.io/argo-helm"
+  chart            = "argo-cd"
+  namespace        = "argocd"
+  version          = "4.9.7"
+  create_namespace = true
+
+  values = [
+    file("./argocd/application.yml")
+  ]
+}
